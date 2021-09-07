6bbab22ae1281cf65c9031741d8e2b45d282435b
omkar2021 <omkarsgurav@gmail.com>
2021-09-07 14:02:10 +0530

    new changes added

--- Apache_POI/src/main/java/com/jbk/readExcel/DemoGit.java
+++ Apache_POI/src/main/java/com/jbk/readExcel/DemoGit.java
@@ -8,8 +8,7 @@ public class DemoGit {
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<Integer>();
         List<String> list2 = new ArrayList<String>();
-        
-        List bc=new ArrayList();
+        List xy=new ArrayList();
 
   	}
 }
