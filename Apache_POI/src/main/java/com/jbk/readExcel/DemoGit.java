59a5165a1acf2427c9ea3c2f7714093a1acd6ecc
omkar2021 <86929116+omkar2021@users.noreply.github.com>
2021-09-07 10:58:02 +0530

    Update DemoGit.java
    Remove List<Integer>

--- Apache_POI/src/main/java/com/jbk/readExcel/DemoGit.java
+++ Apache_POI/src/main/java/com/jbk/readExcel/DemoGit.java
@@ -6,8 +6,6 @@ import java.util.List;
 public class DemoGit {
 
 	public static void main(String[] args) {
-		List<Integer> list = new ArrayList<Integer>();
-        List<String> list2 = new ArrayList<String>();
-
-  	}
+               List<String> list2 = new ArrayList<String>();
+           }
 }
